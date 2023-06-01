4d7aaf09f02a9bf1b6f8a964e6b55c0929cb4fec
Rudeus <stevethomas499@gmail.com>
2023-06-01 18:47:44 +0530

    Fixing site title

--- components/layout.js
+++ components/layout.js
@@ -7,7 +7,7 @@ import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
 const name = 'Rudeus Greyrat'
-export const siteTitle = 'Next.js Sample Website'
+export const siteTitle = 'Rudy\'s Website'
 
 export default function Layout({ children, home }) {
   return (
